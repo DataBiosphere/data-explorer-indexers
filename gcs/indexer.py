369544fd5ef0f5d913d315e9efa35510b1f7b852
Melissa Chang <melissachang@users.noreply.github.com>
2018-07-02 12:54:47 -0700

    Review fixes

--- gcs/indexer.py
+++ gcs/indexer.py
@@ -41,18 +41,18 @@ def parse_args():
     return parser.parse_args()
 
 
-def index_gcs_path(gcs_path):
-    # Input gcs_path looks like
+def index_gcs_pattern(gcs_pattern):
+    # Input gcs_pattern looks like
     # gs://genomics-public-data/platinum-genomes/bam/PRIMARY_KEY_
 
-    logger.info('Processing %s.' % gcs_path)
+    logger.info('Processing %s.' % gcs_pattern)
 
-    trimmed_gcs_path = gcs_path.replace('gs://', '')
+    trimmed_gcs_pattern = gcs_pattern.replace('gs://', '')
 
     # bucket_str looks like genomics-public-data
-    bucket_str = trimmed_gcs_path.split('/')[0]
+    bucket_str = trimmed_gcs_pattern.split('/')[0]
 
-    prefix = trimmed_gcs_path.split('/', 1)[1]
+    prefix = trimmed_gcs_pattern.split('/', 1)[1]
     # prefix looks like platinum-genomes/bam/
     prefix = prefix[:prefix.index('PRIMARY_KEY')]
 
@@ -61,7 +61,7 @@ def index_gcs_path(gcs_path):
     bucket = storage.Client(project=None).bucket(bucket_str)
     objects = bucket.list_blobs(prefix=prefix)
 
-    regex = re.compile(gcs_path.replace('PRIMARY_KEY', '(\w+)'))
+    regex = re.compile(gcs_pattern.replace('PRIMARY_KEY', '(\w+)'))
 
     for obj in objects:
         obj_path = 'gs://%s/%s' % (bucket_str, obj.name)
@@ -80,13 +80,14 @@ def main():
     # Read dataset config files
     index_name = indexer_util.get_index_name(args.dataset_config_dir)
     gcs_config_path = os.path.join(args.dataset_config_dir, 'gcs.json')
-    gcs_paths = indexer_util.parse_json_file(gcs_config_path)['gcs_paths']
+    gcs_patterns = indexer_util.parse_json_file(gcs_config_path)[
+        'gcs_patterns']
 
     es = indexer_util.maybe_create_elasticsearch_index(args.elasticsearch_url,
                                                        index_name)
 
-    for gcs_path in gcs_paths:
-        index_gcs_path(gcs_path)
+    for gcs_pattern in gcs_patterns:
+        index_gcs_pattern(gcs_pattern)
 
 
 if __name__ == '__main__':
