19dbb9e9e4f7fc1d7e56f9c3f3d10b9845418451
Bryan Crampton <bryancrampton@google.com>
2018-09-07 09:10:36 -0700

    Use jq key sorting, write to tmp file to show diff

--- bigquery/tests/integration.sh
+++ bigquery/tests/integration.sh
@@ -2,6 +2,9 @@
 #
 # Run Data Explorer Indexer integration tests.
 #
+# Regenerate the integration_golden_index.json by running from project root (after indexing):
+# curl -s 'http://localhost:9200/1000_genomes/_search?size=3500&sort=_id:asc' | jq -rS '.hits.hits' > 'bigquery/tests/integration_golden_index.json'
+#
 # bigquery/tests/integration.sh <billing_project_id>
 #
 
@@ -22,13 +25,18 @@ BILLING_PROJECT_ID=${billing_project_id} docker-compose up --build indexer
 # For some reason index isn't available right after indexer terminates, so sleep.
 sleep 5
 
-# Compare the index to a golden index file.
-golden_index=$(cat tests/integration_golden_index.json)
-current=$(curl -s 'http://localhost:9200/1000_genomes/_search?size=10000&sort=_id:asc' | jq -r '.hits.hits')
-if [[ "$golden_index" != "$current" ]]; then
-  echo "Index does not match golden json file"
+# Validate the correct number of documents were indexed.
+expr $(curl -s 'http://localhost:9200/1000_genomes/_search' | jq -r '.hits.total') = "3500"
+
+# Write the index out to a file in order to diff.
+curl -s 'http://localhost:9200/1000_genomes/_search?size=3500&sort=_id:asc' | jq -rS '.hits.hits' > 'tests/actual_index.json'
+DIFF=$(diff tests/integration_golden_index.json tests/actual_index.json) 
+if [ "$DIFF" != "" ]; then
+  echo "Index does not match golden json file, diff:"
+  echo "$DIFF"
   exit 1
 fi
 
+rm tests/actual_index.json
 curl -XDELETE localhost:9200/1000_genomes
 docker-compose stop
