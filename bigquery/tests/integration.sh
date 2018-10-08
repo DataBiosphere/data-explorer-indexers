3a9e366a85628be70712de0c2a3d011cd09837fc
Bryan Crampton <bryancrampton@google.com>
2018-10-08 09:03:36 -0700

    Start migration 1000 genomes dataset (#77)

--- bigquery/tests/integration.sh
+++ bigquery/tests/integration.sh
@@ -5,7 +5,7 @@
 # Regenerate golden files by running from bigquery/:
 #   docker-compose up -d elasticsearch
 #   curl -XDELETE localhost:9200/1000_genomes && curl -XDELETE localhost:9200/1000_genomes_fields
-#   BILLING_PROJECT_ID=google.com:api-project-360728701457 docker-compose up --build -d indexer
+#   BILLING_PROJECT_ID=google.com:api-project-360728701457 docker-compose up --build indexer
 #   curl -s 'http://localhost:9200/1000_genomes/type/HG02924' | jq -rS '._source' > 'tests/1000_genomes_golden.json'
 #   curl -s 'http://localhost:9200/1000_genomes/_mappings?pretty' | jq -rS '.' > 'tests/1000_genomes_mappings_golden.json'
 #   curl -s 'http://localhost:9200/1000_genomes_fields/_search?size=200' | jq -rS '.hits.hits' > 'tests/1000_genomes_fields_golden.json'
@@ -45,8 +45,8 @@ sleep 5
 
 # Validate the correct number of documents were indexed.
 DOC_COUNT=$(curl -s 'http://localhost:9200/1000_genomes/_search' | jq -r '.hits.total')
-if [ "$DOC_COUNT" != "3714" ]; then
-  echo "Number of documents is incorrect, expected 3714, got $DOC_COUNT"
+if [ "$DOC_COUNT" != "3500" ]; then
+  echo "Number of documents is incorrect, expected 3500, got $DOC_COUNT"
   exit 1
 fi
 
