1daa8a906d86a9fd0a89a3618cbe4408cd6e0c05
Bryan Crampton <bryancrampton@google.com>
2018-10-08 08:36:36 -0700

    fix test

--- bigquery/tests/integration.sh
+++ bigquery/tests/integration.sh
@@ -10,38 +10,38 @@
 #   curl -s 'http://localhost:9200/1000_genomes/_mappings?pretty' | jq -rS '.' > 'tests/1000_genomes_mappings_golden.json'
 #   curl -s 'http://localhost:9200/1000_genomes_fields/_search?size=200' | jq -rS '.hits.hits' > 'tests/1000_genomes_fields_golden.json'
 
-# if (( $# != 1 ))
-# then
-#   echo "Usage: tests/integration.sh <billing_project_id>"
-#   echo "  where <billing_project_id> is the GCP project billed for BigQuery usage"
-#   echo "Run this script from bigquery/ directory"
-#   exit 1
-# fi
+if (( $# != 1 ))
+then
+  echo "Usage: tests/integration.sh <billing_project_id>"
+  echo "  where <billing_project_id> is the GCP project billed for BigQuery usage"
+  echo "Run this script from bigquery/ directory"
+  exit 1
+fi
 
-# waitForClusterHealthy() {
-#   status=''
-#   # For some reason, cluster health is green on CircleCI
-#   while [[ $status != 'yellow' && $status != 'green' ]]; do
-#     echo "Waiting for Elasticsearch cluster to be healthy"
-#     sleep 1
-#     status=$(curl -s localhost:9200/_cluster/health | jq -r '.status')
-#   done
-#   echo "Elasticsearch cluster is now healthy"
-# }
+waitForClusterHealthy() {
+  status=''
+  # For some reason, cluster health is green on CircleCI
+  while [[ $status != 'yellow' && $status != 'green' ]]; do
+    echo "Waiting for Elasticsearch cluster to be healthy"
+    sleep 1
+    status=$(curl -s localhost:9200/_cluster/health | jq -r '.status')
+  done
+  echo "Elasticsearch cluster is now healthy"
+}
 
 
-# billing_project_id=$1
-# docker network create data-explorer_default
+billing_project_id=$1
+docker network create data-explorer_default
 
-# # Run Elasticsearch in the background with the indexer in the foreground to prevent blocking the main thread.
-# docker-compose up -d elasticsearch
-# waitForClusterHealthy
-# curl -XDELETE localhost:9200/1000_genomes
-# curl -XDELETE localhost:9200/1000_genomes_fields
+# Run Elasticsearch in the background with the indexer in the foreground to prevent blocking the main thread.
+docker-compose up -d elasticsearch
+waitForClusterHealthy
+curl -XDELETE localhost:9200/1000_genomes
+curl -XDELETE localhost:9200/1000_genomes_fields
 
-# BILLING_PROJECT_ID=${billing_project_id} docker-compose up --build indexer
-# # For some reason index isn't available right after indexer terminates, so sleep.
-# sleep 5
+BILLING_PROJECT_ID=${billing_project_id} docker-compose up --build indexer
+# For some reason index isn't available right after indexer terminates, so sleep.
+sleep 5
 
 # Validate the correct number of documents were indexed.
 DOC_COUNT=$(curl -s 'http://localhost:9200/1000_genomes/_search' | jq -r '.hits.total')
