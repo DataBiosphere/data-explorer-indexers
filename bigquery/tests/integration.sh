6809e4efa4703ee140efab97ec9779f282118fa0
Willy Nojopranoto <nojopranoto@gmail.com>
2019-02-12 14:05:54 -0800

    Sort test outputs

--- bigquery/tests/integration.sh
+++ bigquery/tests/integration.sh
@@ -37,7 +37,9 @@ curl -XDELETE localhost:9200/1000_genomes_fields
 
 docker-compose up --build indexer
 # For some reason index isn't available right after indexer terminates, so sleep.
+echo "Indexer successful, sleeping for 5 seconds now"
 sleep 5
+echo "Sleep done"
 
 # Validate the correct number of documents were indexed.
 DOC_COUNT=$(curl -s 'http://localhost:9200/1000_genomes/_search' | jq -r '.hits.total')
@@ -45,6 +47,8 @@ if [ "$DOC_COUNT" != "3500" ]; then
   echo "Number of documents is incorrect, expected 3500, got $DOC_COUNT"
   exit 1
 fi
+
+echo "Diff 1 pass"
 
 # Write the index out to a file in order to diff.
 curl -s 'http://localhost:9200/1000_genomes/type/HG02924' | jq -rS '._source' > 'tests/1000_genomes.json'
@@ -55,6 +59,8 @@ if [ "$DIFF" != "" ]; then
   exit 1
 fi
 
+echo "Diff 2 pass"
+
 # Write the mappings out to a file in order to diff.
 curl -s 'http://localhost:9200/1000_genomes/_mappings' | jq -rS '.' > 'tests/1000_genomes_mappings.json'
 DIFF=$(diff tests/1000_genomes_mappings_golden.json tests/1000_genomes_mappings.json)
@@ -64,8 +70,10 @@ if [ "$DIFF" != "" ]; then
   exit 1
 fi
 
+echo "Diff 3 pass"
+
 # Write the fields index out to a file in order to diff.
-curl -s 'http://localhost:9200/1000_genomes_fields/_search?size=200' | jq -rS '.hits.hits' > 'tests/1000_genomes_fields.json'
+curl -s 'http://localhost:9200/1000_genomes_fields/_search?size=200' | jq -rS '.hits.hits' | jq -r '.|=sort_by(._id)' > 'tests/1000_genomes_fields.json'
 DIFF=$(diff tests/1000_genomes_fields_golden.json tests/1000_genomes_fields.json)
 if [ "$DIFF" != "" ]; then
   echo "Fields index does not match golden json file, diff:"
@@ -73,8 +81,10 @@ if [ "$DIFF" != "" ]; then
   exit 1
 fi
 
+echo "Diff 4 pass"
 
 rm tests/1000_genomes.json
 rm tests/1000_genomes_mappings.json
 rm tests/1000_genomes_fields.json
 docker-compose stop
+echo "Test Success!!"
