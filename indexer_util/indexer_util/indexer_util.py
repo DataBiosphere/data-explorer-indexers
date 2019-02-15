fb1cb7720072f28e08ceae4a43d4c5b12e3f3688
Willy Nojopranoto <nojopranoto@gmail.com>
2019-02-14 16:47:45 -0800

    Willyn/bulk at end (#130)
    Instead of repeated bulks, keep table data in memory before bulking at end.

--- indexer_util/indexer_util/indexer_util.py
+++ indexer_util/indexer_util/indexer_util.py
@@ -132,43 +132,19 @@ def _complete_indexing(es):
     })
 
 
-def bulk_index_scripts(es, index_name, scripts_by_id):
-    # Use generator so we can index arbitrarily large iterators (like tables),
-    # without having to load into memory.
-    def es_actions(scripts_by_id):
-        for _id, script in scripts_by_id:
-            yield ({
-                '_op_type': 'update',
-                '_index': index_name,
-                # type will go away in future versions of Elasticsearch. Just
-                # use any string here.
-                '_type': 'type',
-                '_id': _id,
-                'scripted_upsert': True,
-                'script': script,
-                'upsert': {},
-            })
-
-    _prepare_for_indexing(es)
-    # For large datasets, the default timeout of 10s is sometimes not enough.
-    bulk(es, es_actions(scripts_by_id), request_timeout=300)
-    _complete_indexing(es)
-
-
 def bulk_index_docs(es, index_name, docs_by_id):
-    # Use generator so we can index arbitrarily large iterators (like tables),
-    # without having to load into memory.
+    # docs_by_id is a dict containing documents to be indexed into Elasticsearch.
+    # Key is document id; value is document contents.
     def es_actions(docs_by_id):
-        for _id, doc in docs_by_id:
+        for _id, doc in docs_by_id.iteritems():
             yield ({
-                '_op_type': 'update',
+                '_op_type': 'create',
                 '_index': index_name,
                 # type will go away in future versions of Elasticsearch. Just
                 # use any string here.
                 '_type': 'type',
                 '_id': _id,
-                'doc': doc,
-                'doc_as_upsert': True
+                '_source': doc,
             })
 
     _prepare_for_indexing(es)
